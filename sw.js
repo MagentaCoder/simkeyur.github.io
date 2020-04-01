4d59ba9ba899d45df3d9ae3057a555d6975b3825
Keyur Patel <simkeyur@gmail.com>
2020-03-31 20:52:28 -0500

    added PWA support

---
+++ sw.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="vendor/jquery/jquery.js" />
+var cacheName = 'simkeyur-pwa';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/resume.css',
+    '/js/main.js',
+    '/Resume/KEYUR_Resume_Jan2020.pdf',
+    '/vendor/bootstrap/css/bootstrap.css',
+    '/vendor/bootstrap/js/bootstrap.js',
+    '/vendor/jquery/jquery.js',
+    '/js/resume.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
