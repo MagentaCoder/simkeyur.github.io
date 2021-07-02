381268201e911f698fa31754744d9ad5d031b52b
Keyur Patel <simkeyur@gmail.com>
2021-07-01 22:45:11 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-﻿window.onload = () => {
+window.onload = () => {
     'use strict';
+
+    let installable = true;
+    if (!('serviceWorker' in navigator)){
+      installable = false;
+    }
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./sw.js');
     }
+    
+    let installButton = document.createElement('button');
+    let prompt;
+    window.addEventListener('beforeinstallprompt', function(e){
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      prompt = e;
+    });
+
+    installButton.addEventListener('click', function(){
+       prompt.prompt();
+    })
+
+    window.addEventListener('appinstalled', async function(e) {
+       installButton.style.display = "none";
+    });
+    
 }
+
+
+
